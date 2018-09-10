c6382ce290912e66915a22642af65b257d77ce9a
lefkou <elekousis@gmail.com>
2018-09-10 18:09:57 +0100

    Added pdf downloader

--- get_pdf_from_csv.py
+++ get_pdf_from_csv.py
@@ -1,7 +1,12 @@
-from selenium import webdriver
-driver = webdriver.Chrome()
-driver.get('https://ww2.eagle.org/en/rules-and-resources/rules-and-guides.html#/content/dam/eagle/rules-and-guides/current/generic/list_of_ABS_notations_and_symbols')
-links = driver.find_elements_by_xpath('.//span[@class="file"]/a')
+import requests
+import shutil
+import urllib.request
 
-for link in links:
-    link.get_attribute('href')
+
+with open("pdf_links.csv") as f:
+    for url in f:
+        opener=urllib.request.build_opener()
+        opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
+        urllib.request.install_opener(opener)
+        urllib.request.urlretrieve(url, url.rsplit('/',1)[1])
+
